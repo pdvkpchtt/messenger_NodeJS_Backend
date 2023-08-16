a3d522f0b00fefc38fde7047faba712f0c4f9ebb
Danil <kabirdanilov14@gmail.com>
2023-08-17 01:55:58 +0500

    addet form validation

---
+++ controllers/validateForm.js
@@ -0,0 +1,25 @@
+const Yup = require("yup");
+
+const formSchema = Yup.object({
+  username: Yup.string().required("Username required"),
+  password: Yup.string().required("Password required"),
+});
+
+const validateForm = (req, res) => {
+  const formData = req.body;
+
+  formSchema
+    .validate(formData)
+    .catch((err) => {
+      res.status(422).send();
+      console.log(err.errors);
+    })
+    .then((valid) => {
+      if (valid) {
+        res.status(200).send();
+        console.log("form is good");
+      }
+    });
+};
+
+module.exports = validateForm;
--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const express = require("express");
+const helmet = require("helmet");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const authRouter = require("./routers/authRouter");
+
+const app = express();
+const server = require("http").createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: "true",
+  },
+});
+
+app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use("/auth", authRouter);
+
+// app.get("/", (req, res) => {
+//   res.json("hi");
+// });
+
+io.on("connect", (socket) => {});
+
+server.listen(4000, () => {
+  console.log("Server listenting on port 4000");
+});
